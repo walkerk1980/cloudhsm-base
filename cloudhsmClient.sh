903cf77933474749eaf365914877a27a0fee9f5d
Keith Walker <walkerk1980@users.noreply.github.com>
2019-08-14 13:36:57 -0400

    fix logging

--- cloudhsmClient.sh
+++ cloudhsmClient.sh
@@ -2,4 +2,4 @@
 
 #/usr/sbin/service cloudhsm-client start
 
-/opt/cloudhsm/bin/cloudhsm_client /opt/cloudhsm/etc/cloudhsm_client.cfg
+/opt/cloudhsm/bin/cloudhsm_client /opt/cloudhsm/etc/cloudhsm_client.cfg >> /opt/cloudhsm/run/cloudhsm_client.log
